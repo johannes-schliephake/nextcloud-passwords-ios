406cee99b7302186f3daa0b10d452487bba24681
Johannes Schliephake <mail@cdslash.de>
2021-03-17 18:41:25 +0100

    Include debug indicator in client name

--- Shared/Configuration.swift
+++ Shared/Configuration.swift
@@ -7,7 +7,7 @@ struct Configuration {
     static let appService = Bundle.main.object(forInfoDictionaryKey: "AppService") as! String // swiftlint:disable:this force_cast
     static let appGroup = Bundle.main.object(forInfoDictionaryKey: "AppGroup") as! String // swiftlint:disable:this force_cast
     static let appKeychain = Bundle.main.object(forInfoDictionaryKey: "AppKeychain") as! String // swiftlint:disable:this force_cast
-    static let clientName = "\(Bundle.main.infoDictionary?["CFBundleName"] as! String) (iOS)" // swiftlint:disable:this force_cast
+    static let clientName = "\(Bundle.main.infoDictionary?["CFBundleName"] as! String) (iOS\(appService.hasSuffix("debug") ? ", Debug" : ""))" // swiftlint:disable:this force_cast
     static let isTestEnvironment = ProcessInfo.processInfo.environment["TEST"] == "true"
     static let userDefaults = UserDefaults(suiteName: Configuration.appGroup)!
     
