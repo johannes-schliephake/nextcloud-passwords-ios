de33fe9ed72fc19b3be7dc1e663f547c0f4c90fc
Johannes Schliephake <mail@cdslash.de>
2021-09-17 22:48:05 +0200

    Update SwiftUI previews

--- Shared/Extensions/PreviewDevice+Generate.swift
+++ Shared/Extensions/PreviewDevice+Generate.swift
@@ -5,7 +5,7 @@ extension PreviewDevice {
     
     /// Generate a few interesting preview configurations and insert a view into each
     static func generate<Content: View>(@ViewBuilder content: @escaping () -> Content) -> some View {
-        let deviceNames = ["iPhone 12", "iPhone 12 Pro Max", "iPhone 12 mini", "iPhone SE (2nd generation)", "iPod touch (7th generation)"]
+        let deviceNames = ["iPhone 13 Pro", "iPhone 13 Pro Max", "iPhone SE (2nd generation)", "iPhone 8 Plus", "iPhone SE (1st generation)", "iPad Air (4th generation)"]
         let colorSchemes = ColorScheme.allCases
         let locales = Bundle.main.localizations.map(Locale.init)
         return Group {
@@ -16,6 +16,13 @@ extension PreviewDevice {
                     .previewDisplayName("\(deviceNames.first!) (\(locales.first!.identifier.uppercased()), \(colorScheme))")
                     .environment(\.locale, locales.first!)
                     .preferredColorScheme(colorScheme)
+                    .apply {
+                        view in
+                        if #available(iOS 15, *) {
+                            view
+                                .previewInterfaceOrientation(deviceNames.first!.contains("iPad") ? .landscapeLeft : .portrait)
+                        }
+                    }
             }
             ForEach(locales, id: \.identifier) {
                 locale in
@@ -24,6 +31,13 @@ extension PreviewDevice {
                     .previewDisplayName("\(deviceNames.first!) (\(locale.identifier.uppercased()), \(colorSchemes.first!))")
                     .environment(\.locale, locale)
                     .preferredColorScheme(colorSchemes.first!)
+                    .apply {
+                        view in
+                        if #available(iOS 15, *) {
+                            view
+                                .previewInterfaceOrientation(deviceNames.first!.contains("iPad") ? .landscapeLeft : .portrait)
+                        }
+                    }
             }
             ForEach(deviceNames.dropFirst(), id: \.self) {
                 deviceName in
@@ -32,6 +46,13 @@ extension PreviewDevice {
                     .previewDisplayName("\(deviceName) (\(locales.first!.identifier.uppercased()), \(colorSchemes.first!))")
                     .environment(\.locale, locales.first!)
                     .preferredColorScheme(colorSchemes.first!)
+                    .apply {
+                        view in
+                        if #available(iOS 15, *) {
+                            view
+                                .previewInterfaceOrientation(deviceName.contains("iPad") ? .landscapeLeft : .portrait)
+                        }
+                    }
             }
         }
     }
