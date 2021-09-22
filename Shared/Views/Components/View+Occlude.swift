f1af639c8a68220ac2c0b703a53b5268600e7907
Johannes Schliephake <mail@cdslash.de>
2021-09-22 22:48:24 +0200

    Fix nav bar glitches on iOS 15

--- Shared/Views/Components/View+Occlude.swift
+++ Shared/Views/Components/View+Occlude.swift
@@ -3,10 +3,24 @@ import SwiftUI
 
 extension View {
     
-    func occlude(_ isOccluded: Bool) -> some View {
-        blur(radius: isOccluded ? 20 : 0)
-            .animation(.easeOut(duration: 0.2))
-            .disabled(isOccluded)
+    @ViewBuilder func occlude(_ isOccluded: Bool) -> some View {
+        if #available(iOS 15, *) {
+            ZStack {
+                self
+                    .disabled(isOccluded)
+                Rectangle()
+                    .ignoresSafeArea()
+                    .foregroundStyle(.ultraThinMaterial)
+                    .opacity(isOccluded ? 1 : 0)
+                    .animation(.easeInOut(duration: 0.2))
+            }
+        }
+        else {
+            self
+                .blur(radius: isOccluded ? 20 : 0)
+                .animation(.easeOut(duration: 0.2))
+                .disabled(isOccluded)
+        }
     }
     
 }
