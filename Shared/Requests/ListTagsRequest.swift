2a7da49174de6bf14eb5f6fdd96d69ff38b7dde7
Johannes Schliephake <mail@cdslash.de>
2021-11-25 10:04:47 +0100

    Add tag model and requests

---
+++ Shared/Requests/ListTagsRequest.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+
+struct ListTagsRequest {
+    
+    let session: Session
+    
+}
+
+
+extension ListTagsRequest: NCPasswordsRequest {
+    
+    func send(completion: @escaping ([Tag]?) -> Void) {
+        get(action: "tag/list", session: session, completion: completion)
+    }
+    
+    func decode(data: Data) -> [Tag]? {
+        try? Configuration.jsonDecoder.decode([Tag].self, from: data)
+    }
+    
+}
