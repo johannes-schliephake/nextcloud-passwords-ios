3e35140b4a3d6245e82c53d151802482eb741826
Johannes Schliephake <mail@cdslash.de>
2022-01-07 22:36:44 +0100

    Fix crash on iOS 14

--- Shared/Views/Pages/PasswordDetailPage.swift
+++ Shared/Views/Pages/PasswordDetailPage.swift
@@ -241,7 +241,12 @@ struct PasswordDetailPage: View {
                         ForEach(validTags) {
                             tag in
                             NavigationLink("", tag: .entries(tag: tag), selection: $navigationSelection) {
-                                EntriesPage(entriesController: entriesController, tag: tag, showFilterSortMenu: false)
+                                if #available(iOS 15, *) { /// This insanely dumb workaround (duplicated view) prevents a crash on iOS 14 when an attribute is marked with `@available(iOS 15, *) @FocusState`
+                                    EntriesPage(entriesController: entriesController, tag: tag, showFilterSortMenu: false)
+                                }
+                                else {
+                                    EntriesPageFallback(entriesController: entriesController, tag: tag, showFilterSortMenu: false)
+                                }
                             }
                             .isDetailLink(false)
                         }
