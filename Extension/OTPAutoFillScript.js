d74d56b8171885f870a4de08f6db272ebf3798d0
Johannes Schliephake <mail@cdslash.de>
2022-03-22 21:49:28 +0100

    Improve extension OTP field detection

--- Extension/OTPAutoFillScript.js
+++ Extension/OTPAutoFillScript.js
@@ -1,14 +1,29 @@
 var ExtensionPreprocessingJS = {
     
+    otpFields: function() {
+        const queries = ["input[id*=otp]", "input[name*=otp]", "input[inputmode=numeric]", "input[maxlength=1]", "input[type=tel]", "input[type=text]", "input[type=password]", "input[type=number]"]
+        return queries
+            .map(query => document.querySelectorAll(query))
+            .map(nodeList => Array.from(nodeList))
+            .map(nodes => nodes.filter(node => node.type != "hidden"))
+            .map(nodes => nodes.filter(node => node.value.length == 0))
+            .filter(nodes => nodes.length > 0)
+    }(),
+    
     run: function(arguments) {
         arguments.completionFunction({"url": document.URL})
     },
     
     finalize: function(arguments) {
-        var currentOtp = arguments["currentOtp"]
-        var otpField = document.querySelector("input[id*=otp]") ?? document.querySelector("input[name*=otp]") ?? document.querySelector("input[inputmode=numeric]") ?? document.querySelector("input[type=tel]") ?? document.querySelector("input[type=text]") ?? document.querySelector("input[type=password]") ?? document.querySelector("input[type=number]")
+        const currentOtp = arguments["currentOtp"]
+        const otpField = this.otpFields[0]
         if (currentOtp && otpField) {
-            otpField.value = currentOtp
+            if (otpField.length == currentOtp.length) {
+                otpField.forEach((digitField, index) => digitField.value = currentOtp[index])
+            }
+            else {
+                otpField[0].value = currentOtp
+            }
         }
     }
     
