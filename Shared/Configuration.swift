157d0014af5ea26f5168ea7b271320a35b34bb4d
Johannes Schliephake <mail@cdslash.de>
2022-08-13 11:57:44 +0200

    Fix client name in provider and extension

--- Shared/Configuration.swift
+++ Shared/Configuration.swift
@@ -17,7 +17,8 @@ enum Configuration {
     static let appService = Bundle.main.object(forInfoDictionaryKey: "AppService") as! String // swiftlint:disable:this force_cast
     static let appGroup = Bundle.main.object(forInfoDictionaryKey: "AppGroup") as! String // swiftlint:disable:this force_cast
     static let appKeychain = Bundle.main.object(forInfoDictionaryKey: "AppKeychain") as! String // swiftlint:disable:this force_cast
-    static let clientName = "\(Bundle.main.infoDictionary?["CFBundleName"] as! String) (iOS\(appService.hasSuffix("debug") ? ", Debug" : ""))" // swiftlint:disable:this force_cast
+    /// Make sure to only use ASCII characters for the client name because it is used in HTTP headers
+    static let clientName = "\(Bundle.root.infoDictionary?["CFBundleName"] as! String) (iOS\(appService.hasSuffix("debug") ? ", Debug" : ""))" // swiftlint:disable:this force_cast
     static let isTestEnvironment = ProcessInfo.processInfo.environment["TEST"] == "true"
     static let userDefaults: UserDefaults = {
         let userDefaults = UserDefaults(suiteName: isTestEnvironment ? "test.\(Configuration.appGroup)" : Configuration.appGroup)!
