53aecf93cc6570aa4c6d3bd75a7f3cf910e8e7a4
Johannes Schliephake <mail@cdslash.de>
2026-04-24 23:11:57 +0200

    Update dependencies

--- Shared/UseCases/RandomWordUseCase.swift
+++ Shared/UseCases/RandomWordUseCase.swift
@@ -62,7 +62,7 @@ final class RandomWordUseCase: RandomWordUseCaseProtocol {
                     while let self,
                           self.isRunning {
                         guard let wordData = words.randomElement(using: &randomNumberGenerator),
-                              let word = String(data: wordData, encoding: .utf8) else { // swiftlint:disable:this non_optional_string_data_conversion
+                              let word = String(data: wordData, encoding: .utf8) else {
                             self.state.word = .failure(RandomWordError.cannotParseWord)
                             return
                         }
